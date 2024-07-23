d5e38f2285b5caa73af70f44a72c7746dd211d84
Joe Han <jhoon.han@outlook.com>
2024-07-23 22:56:40 +0100

    i am stupid

--- handlers/index.js
+++ handlers/index.js
@@ -1,107 +1,104 @@
 import {
-    getAllRequest,
-    getRequestById,
-    createRequest,
-    updateRequest,
-    deleteRecord,
-    getRequestByShiftId,
-} from '../db/queries.js';
+  getAllRequest,
+  getRequestById,
+  createRequest,
+  updateRequest,
+  deleteRecord,
+  getRequestByShiftId,
+} from "../db/queries.js";
 
-import {computeShift} from "../scheduler/index.js";
-import {pool} from "../db/index.js";
+import { computeShift } from "../scheduler/index.js";
+import { pool } from "../db/index.js";
 
-const PRIORITY_COMPUTED = 0
+const PRIORITY_COMPUTED = 0;
 
 export const getAllRequestsHandler = async (req, res) => {
-    try {
-        const requests = await getAllRequest();
-        return res.status(200).json({status: 'success', data: requests});
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({status: 'fail', message: 'Server Error'});
-    }
+  try {
+    const requests = await getAllRequest();
+    return res.status(200).json({ status: "success", data: requests });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "fail", message: "Server Error" });
+  }
 };
 
 export const getRequestByRequestIdHandler = async (req, res) => {
-    try {
-        const shiftId = req.params.shift_id;
-        const userId = req.params.user_id;
-        const request = await getRequestById(shiftId, userId);
-        return res.status(200).json(request);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({status: 'fail', message: 'Server Error'});
-    }
+  try {
+    const shiftId = req.params.shift_id;
+    const userId = req.params.user_id;
+    const request = await getRequestById(shiftId, userId);
+    return res.status(200).json(request);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "fail", message: "Server Error" });
+  }
 };
-
 
 export const getRequestByShiftIdHandler = async (req, res) => {
-    try {
-        const shiftId = req.params.shift_id;
-        const request = await getRequestByShiftId(shiftId, userId);
-        return res.status(200).json(request);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({status: 'fail', message: 'Server Error'});
-    }
+  try {
+    const shiftId = req.params.shift_id;
+    const request = await getRequestByShiftId(shiftId, userId);
+    return res.status(200).json(request);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "fail", message: "Server Error" });
+  }
 };
-
 
 export const createRequestHandler = async (req, res) => {
-    const {shift_id, user_id, priority_user} = req.body;
+  const { shift_id, user_id, priority_user } = req.body;
 
-    if (typeof (shift_id) !== 'number' || typeof (user_id) !== 'number') {
-        return res.status(403).json({message: 'Input validation failed'});
-    }
+  if (typeof shift_id !== "number" || typeof user_id !== "number") {
+    return res.status(403).json({ message: "Input validation failed" });
+  }
 
-    try {
-        const request = await createRequest(shift_id, user_id, priority_user, PRIORITY_COMPUTED);
-        return res.status(201).json({status: 'success', data: request});
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({status: 'fail', message: error.message});
-    }
+  try {
+    const request = await createRequest(
+      shift_id,
+      user_id,
+      priority_user,
+      PRIORITY_COMPUTED,
+    );
+    return res.status(201).json({ status: "success", data: request });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ status: "fail", message: error.message });
+  }
 };
 
-
 export const updateRequestHandler = async (req, res) => {
-    const {shift_id, user_id, status} = req.body;
+  const { shift_id, user_id, status } = req.body;
 
-    try {
-        const request = await updateRequest(shift_id, user_id, status);
+  try {
+    const request = await updateRequest(shift_id, user_id, status);
 
-        return res.status(201).json({request});
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message: 'Server Error'});
-    }
+    return res.status(201).json({ request });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 export const deleteRequestHandler = async (req, res) => {
-    try {
-        const shiftId = req.params.shift_id;
-        const userId = req.params.user_id;
-        const request = await deleteRecord(shiftId, userId);
-        return res.status(200).json({status: 'success', data: request});
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({status: 'fail', message: error.message});
-    }
+  try {
+    const shiftId = req.params.shift_id;
+    const userId = req.params.user_id;
+    const request = await deleteRecord(shiftId, userId);
+    return res.status(200).json({ status: "success", data: request });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "fail", message: error.message });
+  }
 };
 
-
 export const getComputedShiftHandler = async (req, res) => {
-    try {
-        const shift_id = req.params.shift_id;
-        const computedShift = await computeShift(shift_id);
+  try {
+    const shift_id = req.params.shift_id;
+    const computedShift = await computeShift(shift_id);
 
-        return res.status(200).json({status: 'success', data: computedShift});
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({status: 'fail', message: error.message});
-    }
-
-}
-
+    return res.status(200).json({ status: "success", data: computedShift });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "fail", message: error.message });
+  }
+};
--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-import express from 'express';
-import appRouter from './routes/index.js';
-import {connectToDatabase} from './db/index.js';
+import express from "express";
+import appRouter from "./routes/index.js";
+import { connectToDatabase } from "./db/index.js";
+import cors from "cors";
 
 const app = express();
 
 // middlewares
 app.use(express.json());
 
-app.use('/api/v1/request', appRouter);
+// Enable All CORS Requests
+app.use(cors());
+
+app.use("/api/v1/request", appRouter);
 
 const PORT = process.env.PORT || 5001;
 
 const server = app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+  console.log("Server is running on port", PORT);
 });
 
 connectToDatabase()
-    .then(() => {
-        console.log('Connected to database');
-    })
-    .catch((error) => {
-        console.error('Failed to connect to database', error);
-        server.close(() => {
-            process.exit(0);
-        });
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to database", error);
+    server.close(() => {
+      process.exit(0);
     });
+  });
 
-process.on('unhandledRejection', (err) => {
-    console.error('UNHANDLED REJECTION! 💥 Shutting down...');
-    server.close(() => {
-        process.exit(0);
-    });
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION! 💥 Shutting down...");
+  server.close(() => {
+    process.exit(0);
+  });
 });
-process.on('SIGTERM', () => {
-    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-    server.close(() => {
-        console.log('💥 Process terminated!');
-    });
+process.on("SIGTERM", () => {
+  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+  server.close(() => {
+    console.log("💥 Process terminated!");
+  });
 });
-process.on('SIGINT', () => {
-    console.log('👋 SIGINT RECEIVED. Shutting down gracefully');
-    server.close(() => {
-        console.log('💥 Process terminated!');
-    });
+process.on("SIGINT", () => {
+  console.log("👋 SIGINT RECEIVED. Shutting down gracefully");
+  server.close(() => {
+    console.log("💥 Process terminated!");
+  });
 });
